e64321c761db18a46feda64ffcdb24ff31d9be01
Pavel Kulyov <kulyov.pavel@gmail.com>
2025-11-03 22:10:53 +0300

    Add temporary stub assets

---
+++ assets/maps/terrain-l3.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="terrain" tilewidth="128" tileheight="144" tilecount="18" columns="6" tilerendersize="grid">
+ <tileoffset x="0" y="-32"/>
+ <image source="terrain.png" trans="008080" width="768" height="432"/>
+</tileset>
---
+++ assets/maps/terrain-o1px.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="terrain-o1px" tilewidth="128" tileheight="144" tilecount="12" columns="3" tilerendersize="grid">
+ <image source="terrain-o1px.png" trans="ff00ff" width="384" height="576"/>
+</tileset>
